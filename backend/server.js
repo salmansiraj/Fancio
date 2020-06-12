bbca7f655d8c3f8cb94c0fffa41424b17dc18217
Salman Siraj <ss10298@nyu.edu>
2020-06-12 00:59:43 -0400

    adding mongodb config

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const path = require('path')
 // Using mongoose to help connect to MongoDB
 const mongoose = require('mongoose')
 
-require('dotenv').config()
+require('dotenv').config({ path: '.env'})
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -14,8 +14,12 @@ app.use(cors())
 app.use(express.json())
 
 // Input mongoClusterURL from ATLAS to connect 
-const uri = process.env.ATLAS_URI
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+mongoose.connect(process.env.ATLAS_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
 
 // Connecting DB to backend server 
 const connection = mongoose.connection
